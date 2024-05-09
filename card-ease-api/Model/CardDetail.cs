3f1e9f5b2283baa4bc3081549ab24f57b024b5c4
Hanna Kołtuniak <hanna.koltuniak@gmail.com>
2024-05-09 14:39:43 +0200

    init

---
+++ card-ease-api/Model/CardDetail.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace card_ease_api.Model
+{
+    public class CardDetail
+    {
+        [Key]
+        public int PaymentDetailId { get; set; }
+
+        [Column(TypeName = "nvarchar(150)")]
+        public string CardOwnerName { get; set; } = "";
+
+        [Column(TypeName = "nvarchar(16)")]
+        public string CardNumber { get; set; } = "";
+
+        //mm/yy
+        [Column(TypeName = "nvarchar(5)")]
+        public string ExpirationDate { get; set; } = "";
+
+        [Column(TypeName = "nvarchar(3)")]
+        public string SecurityCode { get; set; } = "";
+    }
+}
